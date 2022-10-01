d922aa9d6af748d429c467b3af38a115c2015642
Sejal Patel <102937879+sejalpatelll@users.noreply.github.com>
2022-10-01 11:52:58 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,3 +23,46 @@ function App() {
 }
 
 export default App;
+
+let budget = (function() { // constructor
+
+  let App = function(category, amount) {
+      this.category = category;
+      this.amount = amount;
+      this.piChart = -1; // -1= does not exist
+  };
+
+// creating a calculator to add expenses and income added by a user and display the total
+const calculator = function(income, expenses, total) {
+  let income = 0;
+  let expenses = 0;
+  // let category = "";
+  let total = 0;
+  let addIncome = function() {
+    income += income;
+  }
+  let addExpenses = function() {
+    expenses += expenses;
+  }
+  let addTotal = function() {
+    total = income - expenses;
+  }
+  return {
+    addIncome,
+    addExpenses,
+    addTotal
+  }
+}
+
+
+// create a function that adds and sets a categories to the expense 
+const addCategory = function(category) {
+  let category = "";
+  let addCategory = function() {
+    category = category;
+  }
+  return {
+    addCategory
+  }
+};
+
